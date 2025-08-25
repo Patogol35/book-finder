dbf14bfc220c73b49b9c170e96630826e7eb0e33
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-08-24 21:49:13 -0500

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -6,10 +6,10 @@ export default function FavoritesPage() {
   const { favorites } = useContext(FavoritesContext);
 
   return (
-    <div className="container">
-      <h1>⭐ Mis Favoritos</h1>
+    <div className="p-6 container">
+      <h1 className="text-2xl font-bold mb-4">Mis Favoritos</h1>
       {favorites.length === 0 ? (
-        <p className="empty">No tienes libros guardados aún.</p>
+        <p className="empty">No tienes libros guardados.</p>
       ) : (
         <div className="books-grid">
           {favorites.map(book => (
